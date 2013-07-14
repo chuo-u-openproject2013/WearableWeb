a5f8c7cd97d0394cfac8395cb45d67b882d277e2
0x5A4D <0x5A4D@mail>
2013-07-15 01:43:25 +0900

    add www

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="main.css" rel="stylesheet" type="text/css">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
