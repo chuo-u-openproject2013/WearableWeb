0c5634b99cc7c8d1b2282ede77aa74007a1e6e86
0x5A4D <0x5A4D@mail>
2013-08-11 23:21:17 +0900

    再構築

--- www/index.php
+++ www/index.php
@@ -1,9 +1,8 @@
 <?php
-require_once 'lib/request_lwws.php';
 require_once 'lib/func.php';
 
 if (isset($_POST['city']) && $_POST['city']!=''){
-    $lwws_ary = request_lwws($_POST['city']);
+    
 }
 ?>
 
@@ -19,16 +18,10 @@ if (isset($_POST['city']) && $_POST['city']!=''){
     </head>
     <body>
 	<h1>本日の服装は…？</h1>
-        <form action="index.php" name="CityForm" method="post">
+        <form action="index.php" name="CitySelectForm" method="post">
                     <label>都道府県:</label>
                     <select name="pref" onchange="Select(this)">
                         <option value="" selected></option>
-                        <?php
-                        $pref = list_pref();
-                        foreach ($pref as $val){
-                            echo '<option value="'.$val.'">'.$val.'</option>'."\n";
-                        }
-                        ?>
                     </select>
                     <label>都市:</label>
                     <select name="city">
@@ -37,31 +30,6 @@ if (isset($_POST['city']) && $_POST['city']!=''){
                     <input type="submit" value="送信">
         </form>
         
-        <?php if(isset($lwws_ary)){ ?>
-        <h2>天気予報</h2>
-        <?php
-            echo '<h3>'.$lwws_ary['location']['prefecture'].' - '.$lwws_ary['location']['city'].'</h3>';
-            echo '<table border="1" cellpadding="2">';
-            echo '<tr> <td></td><td>日付</td><td>予報</td><td>最低/最高気温</td><td>画像</td></tr>';
-            foreach($lwws_ary['forecasts'] as $forecasts=>$idx){
-                echo '<tr><td>'.$idx['dateLabel'].'</td>';
-                echo '<td>'.$idx['date'].'</td>';
-                echo '<td>'.$idx['telop'].'</td>';
-                echo '<td>'.$idx['temperature']['min']['celsius'].'/'.$idx['temperature']['max']['celsius'].'</td>';
-                echo '<td><img src="'.$idx['image']['url'].'" ></td></tr>';
-            }
-            echo '</table>';
-         ?>
-         <p>Powered by <a href="http://weather.livedoor.com/weather_hacks/webservice">livedoor 天気情報</a></p>
-        
-         <h2>Debug</h2>
-         <pre>
-         <?php print_r($lwws_ary); ?>
-         </pre>
-         
-         <h2>快適な服装！</h2>
-         <p style="font-size: 1.5em;color: red;">工事中！！！！</p>
-         <?php } ?>
 
     </body>
 </html>
---
+++ www/lib/owm_api.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * OpenWeatherMap API
+ * http://openweathermap.org/API 
+ */
+
+function getCityidByName($name){
+    
+}
+
+function getForecast($cityid){
+    
+}
+
+?>
+
